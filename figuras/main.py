8bc605c9bc1ef00b2885fb6f1705fafd5d7f6a77
jean.huby <50341023+jeanpaulHT@users.noreply.github.com>
2019-06-02 18:53:05 -0700

    S

--- figuras/main.py
+++ figuras/main.py
@@ -4,7 +4,7 @@ exec(open(str(os.getcwd())+"\\"+"lector.py").read())
 def comparacion(matriz_1,matriz_2):
     if (matriz_1[0]==matriz_2[0]):
         fila=int(matriz_1[0][0])
-        columna=int(matriz_1[0][1])
+        columna=int(matriz_1[0][1])-1
         area=fila*columna
         temp=area
         for i in range(1,fila+1):
