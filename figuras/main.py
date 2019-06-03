d1a1334004afc85090be1c87e4d5cbb9031674cc
jean.huby <50341023+jeanpaulHT@users.noreply.github.com>
2019-06-02 17:20:05 -0700

    a

--- figuras/main.py
+++ figuras/main.py
@@ -1,8 +1,8 @@
 import os
 exec(open(str(os.getcwd())+"\\"+"lector.py").read())
 
-if(lineas_1[0]==lineas_2[0] ):
+def comparacion(matriz_1,matriz_2):
+    if (matriz_1[0]==matriz_2[0]):
     
-
-else:
-    print("La copia difiere de la original.")
+    else:
+        print("La copia difiere de la original.")
